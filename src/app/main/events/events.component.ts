6cc999d710d201efb4d6ebc85fa477073a5ea28f
boycov <boec.n1@mail.ru>
2021-09-25 05:19:34 +0300

    добавлене мероприятия и добавленние изображения

--- src/app/main/events/events.component.ts
+++ src/app/main/events/events.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
+  addEvent = true;
+  avatarImage = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  uploadImage(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        this.avatarImage = event.target.result;
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+  addEvents() {
+    this.addEvent = true;
   }
 
 }
